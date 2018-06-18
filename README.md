7aaa8716de72e40881a3a37b6742c6b61f4f2931
Dave Willenberg <dave@detroit-english.de>
2018-06-18 02:25:03 +0200

    init lol

---
+++ README.md
@@ -0,0 +1,27 @@
+# haveibeenpwned-zxcvbn-lambda-api
+Here's a chance to have your very own serverless API endpoint up on AWS Lambda so you can score new user passwords with the Dropbox's fantastic `zxcvbn` library _AND_ check their password against Troy Hunt's [haveibeenpwned](https://haveibeenpwned.com/) database via his APIv2 Range Query endpoint.
+
+## Lazy quick-start
+1. Create an AWS profile with IAM full access, Lambda full access and API Gateway Administrator privileges.
+2. Add the keys to your ~/.aws/credentials file:
+    ```
+    [pwnage]
+    aws_access_key_id = YOUR_ACCESS_KEY
+    aws_secret_access_key = YOUR_ACCESS_SECRET
+    ```
+3. Rename `example.env.json` to `env.json` and change the values to whatever suits your fancy.
+4. Install dependencies with `npm install`
+5. Launch 🚀 with `npm run deploy`
+6. Change whatever you need to change in the AWS API Gateway to make this work with your own application.
+7. Update the Lambda API with any changes you make to the source by running `npm run update`
+
+This uses the cool-as-a-cucumber [claudia.js](https://claudiajs.com/documentation.html) for handling AWS deployment - please refer to the claudia.js docs to learn more about this voodoo.
+
+### Good to Know
+The health-check endpoint `/_up` is included by default; this also serves as a handy means to warm-up a Lambda function container before your users start feeding you input.
+
+### Disclaimer
+I am not affiliated with Amazon, Troy Hunt, Dropbox, haveibeenpwned or any combination thereof. People just seemed to think that this was neat, and this was the easiest way I could come up with to share it.
+
+### License
+Licensed under [GLWTPL](https://raw.githubusercontent.com/me-shaon/GLWTPL/master/LICENSE)
---
+++ src/index.js
@@ -0,0 +1,82 @@
+﻿'use strict'
+import '@babel/polyfill'
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import helmet from 'helmet'
+import zxcvbn from 'zxcvbn'
+import axios from 'axios'
+import crypto from 'crypto'
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.enable('trust proxy')
+
+app.use(helmet())
+
+const router = express.Router()
+
+const route = r => path.normalize(`${routePrefix}/${r}`)
+
+const endpoint = path.normalize(`/` + (process.env.SCORING_ENDPOINT || '/_score'))
+
+// Health check and/or warm-up endpoint for the function container
+router.get('/_up', (req, res) => res.status(200).json({ ok: true }))
+
+// Password scoring and haveibeenpwned crosscheck endpoint
+router.post(endpoint, async (req, res) => {
+  const { password } = req.body
+  let message, pwned, ok
+  const pwnedUrl = p => `https://api.pwnedpasswords.com/range/${p}`
+  let { score } = zxcvbn(password)
+  try {
+    pwned = await pwnedPassword(password)
+    ok = true
+  } catch (err) {
+    req.log.error(err)
+    ok = false
+    pwned = 1
+    message = err.message
+  }
+  if (pwned) score = 0
+  return res.status(200).json({ ok, score, pwned, message })
+
+  async function pwnedPassword(pw) {
+    const hash = Array.from(
+      await crypto
+        .createHash('sha1')
+        .update(pw)
+        .digest('hex')
+        .toUpperCase()
+    )
+    const prefix = hash.splice(0, 5).join('')
+    const suffix = hash.join('')
+    let result = await axios({
+      url: pwnedUrl(prefix),
+      method: 'GET',
+    })
+      .then(result => result.data)
+      .catch(err => {  // something done goofed
+        throw new Error(`Unable to check password pwnage`)
+      })
+
+    if (!result.includes(suffix)) {
+      return 0
+    }
+
+    result = result.split('\r\n')
+    const match = result.find(r => r.includes(suffix))
+    const hits = match.split(':')[1]
+    return +hits
+  }
+})
+
+// ====================================================
+const routePrefix = process.env.ROUTE_PREFIX || `/`
+app.use(path.normalize(`/` + routePrefix), router)
+
+// =============================================================================
+
+module.exports = app
